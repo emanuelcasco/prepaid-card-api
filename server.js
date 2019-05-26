43b56c43082b7776727fca79e548f8f21a135607
Emanuel Casco <cascoemanuel@gmail.com>
2019-05-25 20:24:19 -0400

    Implement clustering

--- server.js
+++ server.js
@@ -1,14 +1,40 @@
-const app = require('./app'),
-  config = require('./config'),
-  logger = require('./app/logger');
+const cluster = require('cluster');
+const os = require('os');
+
+const app = require('./app');
+const config = require('./config');
+const logger = require('./app/logger');
 
 const port = config.common.api.port || 8080;
 
-Promise.resolve()
+const runExpressServer = () => {
+  return Promise.resolve()
+    .then(() => {
+      app.listen(port);
+      logger.info(`Listening on port: ${port}`);
+    })
+    .catch(logger.error);
+};
 
-  .then(() => {
-    app.listen(port);
+// Check if current process is master.
+if (cluster.isMaster) {
+  // Get total CPU cores.
+  const cpuCount = os.cpus().length;
 
-    logger.info(`Listening on port: ${port}`);
-  })
-  .catch(logger.error);
+  // Spawn a worker for every core.
+  for (let j = 0; j < cpuCount; j++) {
+    cluster.fork();
+  }
+} else {
+  // This is not the master process, so we spawn the express server.
+  runExpressServer();
+}
+
+// Cluster API has a variety of events.
+// Here we are creating a new process if a worker die.
+cluster.on('exit', worker => {
+  logger.info(`Worker ${worker.id} died'`);
+  logger.info('Staring a new one...');
+
+  cluster.fork();
+});
